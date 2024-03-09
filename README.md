348d199706fd9f3423b0d5ee03ebe7e6142cfd6d
Ashish Padhy <100484401+Shurtu-gal@users.noreply.github.com>
2024-03-09 19:34:41 +0530

    chore: setup inital service

---
+++ README.md
@@ -0,0 +1,2 @@
+# local-storage-mgmt-prototype-amplication
+This is a sample project made during amplication internship to research on localstorage integration.
---
+++ server/src/user/base/PromoteUserArgs.ts
@@ -0,0 +1,36 @@
+import { ObjectType, Field } from "@nestjs/graphql";
+import { PromoteUserInput } from "./PromoteUserInput";
+import { ApiProperty } from "@nestjs/swagger";
+import { Type } from "class-transformer";
+import { UserWhereUniqueInput } from "./UserWhereUniqueInput";
+
+@ObjectType()
+class PromoteUserArgs {
+    @Field(() => PromoteUserInput, {
+        nullable: true
+    })
+    @ApiProperty({
+        required: false,
+        type: () => PromoteUserInput
+    })
+    @Type(() => PromoteUserInput)
+    data?: PromoteUserInput;
+
+    @Field(() => UserWhereUniqueInput)
+    @ApiProperty({
+        required: true,
+        type: () => UserWhereUniqueInput
+    })
+    @Type(() => UserWhereUniqueInput)
+    where!: UserWhereUniqueInput;
+
+    @Field(() => UserWhereUniqueInput)
+    @ApiProperty({
+        required: true,
+        type: () => UserWhereUniqueInput
+    })
+    @Type(() => UserWhereUniqueInput)
+    query!: UserWhereUniqueInput;
+}
+
+export { PromoteUserArgs as PromoteUserArgs };
